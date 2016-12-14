aa19f98e66404ad6a776379c96ab522ca3c83276
Kyle Phillips <kylephillips@google.com>
2016-12-14 12:27:01 -0800

    adds options.beforeEnter and options.beforeExit, fixes disabled attribute

--- src/states.js
+++ src/states.js
@@ -28,5 +28,6 @@ export const ERROR_NO_PRESENTABLE_DISPLAYS = "error-no-presentable-displays";
 export const ERROR_BROWSER_NOT_SUPPORTED = "error-browser-not-supported";
 export const ERROR_REQUEST_TO_PRESENT_REJECTED = "error-request-to-present-rejected";
 export const ERROR_EXIT_PRESENT_REJECTED = "error-exit-present-rejected";
+export const ERROR_REQUEST_STATE_CHANGE_REJECTED = "error-request-state-change-rejected";
 
 export const ERROR_UNKOWN = "error-unkown";
